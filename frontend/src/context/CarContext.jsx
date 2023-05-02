2ba48912c2b338cd4c9a78213575d9e69cca7060
tesfayegirma360 <tesfayegirma360@gmail.com>
2023-05-02 16:45:23 +0300

    car api

--- frontend/src/context/CarContext.jsx
+++ frontend/src/context/CarContext.jsx
@@ -2,95 +2,94 @@ import React, { createContext, useContext, useState } from "react";
 import { useQuery, useMutation } from "react-query";
 import PropTypes from "prop-types";
 import ToastContext from "src/context/hot-toast-context/HotToastContext";
-import { GetCars, CreateCar, UpdateCar, DeleteCar } from "src/apis/CarsApi";
-import { BeatLoader } from "react-spinners";
+import { GetCar, CreateCar, UpdateCar, DeleteCar } from "src/apis/CarsApi";
 
 const CarContext = createContext({});
 
 export default CarContext;
 
 export const CarProvider = ({ children }) => {
-     const [createOpen, setCreateOpen] = React.useState(false);
-     const [selectedData, setSelectedData] = React.useState(null);
-     const [editable, setEditable] = React.useState(false);
-     const name = "Cars";
-     const { showToast } = React.useContext(ToastContext);
-     const handleRowClick = (params) => {
-       console.log(params);
-       setSelectedData(params);
-       console.log(selectedData);
-     };
+  const [createOpen, setCreateOpen] = React.useState(false);
+  const [selectedData, setSelectedData] = React.useState(null);
+  const [editable, setEditable] = React.useState(false);
+  const name = "Cars";
+  const { showToast } = React.useContext(ToastContext);
+  const handleRowClick = (params) => {
+    console.log(params);
+    setSelectedData(params);
+    console.log(selectedData);
+  };
 
-     // GetUers
-    const {
+  // GetUers
+  const {
     data: carData,
     isLoading,
     error,
     refetch,
-    } = useQuery(name, GetCars, {
+  } = useQuery(name, GetCar, {
     staleTime: 0,
-    });
-// CreateCar
-const { mutateAsync: createCar } = useMutation(CreateCar, {
-  onSuccess: () => {
-    console.log("Car updated successfully");
-    setCreateOpen(false);
+  });
+  // CreateCar
+  const { mutateAsync: createCar } = useMutation(CreateCar, {
+    onSuccess: () => {
+      console.log("Car updated successfully");
+      setCreateOpen(false);
 
-    showToast("Car created successfully", "success", 2000);
-    refetch();
-  },
-  onError: (err) => {
-    console.log("Car updated successfully");
-    showToast(err.message, "error");
-  },
-});
+      showToast("Car created successfully", "success", 2000);
+      refetch();
+    },
+    onError: (err) => {
+      console.log("Car updated successfully");
+      showToast(err.message, "error");
+    },
+  });
 
-// UpdateCar
-const { mutateAsync: updateCar } = useMutation(UpdateCar, {
-  onSuccess: () => {
-    showToast("Car updated successfully", "success");
-    setSelectedData(null);
-    refetch();
-  },
-  onError: (err) => {
-    showToast(err.message, "error");
-  },
-});
+  // UpdateCar
+  const { mutateAsync: updateCar } = useMutation(UpdateCar, {
+    onSuccess: () => {
+      showToast("Car updated successfully", "success");
+      setSelectedData(null);
+      refetch();
+    },
+    onError: (err) => {
+      showToast(err.message, "error");
+    },
+  });
 
-// DeleteCar
-const { mutateAsync: deleteCar } = useMutation(DeleteCar, {
-  onSuccess: () => {
-    showToast("Car deleted successfully", "success");
-    setSelectedData(null);
-    refetch();
-  },
-  onError: (err) => {
-    showToast(err.message, "error");
-  },
-});
+  // DeleteCar
+  const { mutateAsync: deleteCar } = useMutation(DeleteCar, {
+    onSuccess: () => {
+      showToast("Car deleted successfully", "success");
+      setSelectedData(null);
+      refetch();
+    },
+    onError: (err) => {
+      showToast(err.message, "error");
+    },
+  });
 
-return (
-  <CarContext.Provider
-    value={{
-      carData,
-      isLoading,
-      error,
-      refetch,
-      createOpen,
-      setCreateOpen,
-      selectedData,
-      setSelectedData,
-      handleRowClick,
-      editable,
-      setEditable,
-      createCar,
-      updateCar,
-      deleteCar,
-    }}
-  >
-    {children}
-  </CarContext.Provider>
-);
+  return (
+    <CarContext.Provider
+      value={{
+        carData,
+        isLoading,
+        error,
+        refetch,
+        createOpen,
+        setCreateOpen,
+        selectedData,
+        setSelectedData,
+        handleRowClick,
+        editable,
+        setEditable,
+        createCar,
+        updateCar,
+        deleteCar,
+      }}
+    >
+      {children}
+    </CarContext.Provider>
+  );
 };
 
 CarProvider.propTypes = {
